b196824e699e0da9f7d41765c286cadf95b6b235
Ritesh Bhavsar <ritesh.bhavsar86@gmail.com>
2021-06-21 18:29:36 +0530

    tried for failed test case

--- libJar/src/test/java/com/riteshb/libjar/MyLibClassTest.kt
+++ libJar/src/test/java/com/riteshb/libjar/MyLibClassTest.kt
@@ -8,18 +8,18 @@ import org.junit.runner.RunWith
 //@RunWith(MockitoJUnitRunner::class)
 internal class MyLibClassTest{
 
-//    @Test
+    @Test
     fun `additionInt success`(){
         var param1 : Int = 4
         var param2 : Int = 3
         // ...then the result should be the expected one.
         assertThat(7, `is`(param1+param2))
     }
-//    @Test
-//    fun `additionInt failed`(){
-//        var param1 : Int = 4
-//        var param2 : Int = 3
-//        // ...then the result should be the expected one.
-//        assertThat(7, `is`(param1+param2))
-//    }
+    @Test
+    fun `additionInt failed`(){
+        var param1 : Int = 4
+        var param2 : Int = 3
+        // ...then the result should be the expected one.
+        assertThat(7, `is`(param1+param2))
+    }
 }
